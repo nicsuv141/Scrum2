f1678a75ab73171014354720376849adecf3bae3
Gustav <flamingobajja@hotmail.com>
2017-01-26 11:39:59 +0100

    Test

---
+++ Scrum2/Datalayer/Models/BlogEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalayer.Models
+{
+    class BlogEntry
+    {
+        public int BlogEntryId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public virtual User Author { get; set; }
+        public virtual File Attachment { get; set; }
+        public virtual HashSet<BlogEntryComment> Comments { get; set; }
+    }
+}
