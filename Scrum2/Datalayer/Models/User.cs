deaac846cc5c0140d0583a2c4fd95985ae650ef2
asapers <asasahl@gmail.com>
2017-01-26 13:07:48 +0100

    skapat migrations.

--- Scrum2/Datalayer/Models/User.cs
+++ Scrum2/Datalayer/Models/User.cs
@@ -13,6 +13,7 @@ namespace Datalayer.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string Telephone { get; set; }
     }
 }
